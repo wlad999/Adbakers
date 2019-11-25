b0eb38d7bebff8b9d95b0cbcb4f93f2f61c0aaa5
ваше имяwlad999 <wladmail25@gmeil.com>
2019-11-25 17:20:03 +0200

    Updates

---
+++ precache-manifest.53df3b26477ba19e06286f30570e03e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b81c4eb0ea5de27a536b5c8ed4099a41",
+    "url": "/Adbakers/adbakers/index.html"
+  },
+  {
+    "revision": "1548f30e05f76e0d0162",
+    "url": "/Adbakers/adbakers/static/css/main.4cf1fcb8.chunk.css"
+  },
+  {
+    "revision": "cb538a883bba0937440a",
+    "url": "/Adbakers/adbakers/static/js/2.f766edbd.chunk.js"
+  },
+  {
+    "revision": "1548f30e05f76e0d0162",
+    "url": "/Adbakers/adbakers/static/js/main.012e766d.chunk.js"
+  },
+  {
+    "revision": "24800af7b3b2acff7ec5",
+    "url": "/Adbakers/adbakers/static/js/runtime~main.939e3508.js"
+  }
+]);
